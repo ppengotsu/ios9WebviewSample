965864b0399638034e2851b8fbbc2e8866fbc84f
ppengotsu <ppengotsu.facebook@gmail.com>
2015-09-07 02:07:48 +0900

    一通り実装

---
+++ ios9WebviewSample/PPWKWebViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PPWKWebViewController.swift
+//  ios9WebviewSample
+//
+//  Created by ppengotsu on 2015/09/07.
+//  Copyright © 2015年 ppengotsu. All rights reserved.
+//
+
+
+import UIKit
+import WebKit
+
+class PPWKWebViewController: UIViewController{
+    
+    var webView: WKWebView = WKWebView()
+    var viewUrlString: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //ボタン
+        let closeButton: UIButton = UIButton();
+        closeButton.frame = CGRectMake(self.view.frame.width - 100, 50, 100, 50)
+        closeButton.setTitle("閉じる", forState:UIControlState.Normal)
+        closeButton.addTarget(self, action: "touchedCloseButton:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(closeButton)
+        
+        //WebView
+        self.webView.frame = CGRect(x: 0, y: 100, width: self.view.bounds.width, height: self.view.bounds.height - 100)
+        //webview.delegate = self;
+        self.view.addSubview(self.webView)
+        
+        
+        let url: NSURL = NSURL(string: viewUrlString)!
+        let urlRequest: NSURLRequest = NSURLRequest(URL: url)
+        self.webView.loadRequest(urlRequest)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func setURLString(urlString: String){
+        
+        viewUrlString = urlString
+    }
+    
+    
+    internal func touchedCloseButton(sender:UIButton){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- ios9WebviewSample/ViewController.swift
+++ ios9WebviewSample/ViewController.swift
@@ -7,17 +7,149 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var urlTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        urlTextField.delegate = self;
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: テキストフィールド
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: URLチェック
+    /**
+    urlとして正しいか
+    
+    :returns: 正しければtrue
+    */
+    func isInputURL(let urlString:String) -> Bool{
+        NSLog("URL=%@", urlString)
+        
+        if (urlString .hasPrefix("http://")
+            || urlString .hasPrefix("https://")){
+            
+                return true
+        }
+        
+        //満たない場合
+        return false
+    }
+    
+    // MARK: アラート
+    func showUrlAlert(){
+        let alertController:UIAlertController = UIAlertController(title: "URL不備",
+            message: "URLの確認してください。",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        
+        
+        let cancelAction:UIAlertAction = UIAlertAction(title: "はい",
+            style: UIAlertActionStyle.Cancel,
+            handler: {
+                (action:UIAlertAction!) -> Void in
+        })
+        
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: ボタン押した時
+    
+    /**
+    UIWebViewボタンを押したとき
+    
+    :param: sender
+    */
+    @IBAction func touchedUIWebViewButton(sender: AnyObject){
+        let urlString:String = urlTextField.text!
+        if (isInputURL(urlString) == false){
+            //URLが正しくない
+            showUrlAlert()
+            return
+        }
+        
+        let vc: PPUIWebViewController =  PPUIWebViewController()
+        vc.setURLString(urlString)
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    /**
+    WKWebViewボタンを押したとき
+    
+    :param: sender
+    */
+    @IBAction func touchedWKWebViewButton(sender: AnyObject){
+        let urlString:String = urlTextField.text!
+        if (isInputURL(urlString) == false){
+            //URLが正しくない
+            showUrlAlert()
+            return
+        }
+        
+        let vc: PPWKWebViewController =  PPWKWebViewController()
+        vc.setURLString(urlString)
+        presentViewController(vc, animated: true, completion: nil)
+        
+        
+    }
+    
+    /**
+    SFSafariViewControllerボタンを押したとき
+    
+    :param: sender
+    */
+    @IBAction func touchedSFSafariViewControllerButton(sender: AnyObject){
+        let urlString:String = urlTextField.text!
+        if (isInputURL(urlString) == false){
+            //URLが正しくない
+            showUrlAlert()
+            return
+        }
+        
+        let safariVC = SFSafariViewController(URL: NSURL(string: urlString)!)
+        
+        
+        presentViewController(safariVC, animated: true, completion: nil)
+    }
+    
+    /**
+    標準Safariボタンを押したとき
+    
+    :param: sender
+    */
+    @IBAction func touchedNormalSafariButton(sender: AnyObject){
+        let urlString:String = urlTextField.text!
+        if (isInputURL(urlString) == false){
+            //URLが正しくない
+            showUrlAlert()
+            return
+        }
+        
+        UIApplication.sharedApplication().openURL(NSURL(string: urlString)!)
+        
+        
     }
 
 
